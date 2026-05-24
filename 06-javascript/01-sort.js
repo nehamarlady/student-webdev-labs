7a8ab42bce0e0d1c3d715bf9947065d480dc8c8a
nehamarlady <nehamarlady@gmail.com>
2026-05-24 10:26:25 -0700

    01 sorted complete code

--- 06-javascript/01-sort.js
+++ 06-javascript/01-sort.js
@@ -22,8 +22,22 @@ const pokemons = [
 ];
 
 const sortPokemons = function logSortedPokemons(sortType) {
-  const sortedPokemons = [];
-  // Add your code here
+  const sortedPokemons = [...pokemons];
+
+  switch(sortType){
+    case 'numerically, in ascending order':
+      sortedPokemons.sort((a,b) => a.id - b.id);
+      break;
+    case 'numerically, in descending order':
+      sortedPokemons.sort((a,b) => b.id - a.id);
+      break;
+    case 'alphabetically, in ascending order':
+      sortedPokemons.sort((a,b) => a.name.localeCompare(b.name));
+      break;
+    case 'alphabetically, in descending order':
+      sortedPokemons.sort((a,b) => b.name.localeCompare(a.name));
+      break;
+  }
 
   return sortedPokemons;
 };
