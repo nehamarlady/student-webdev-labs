60a0e9680be6f1d5f3541e1a8eb25e971ca7239a
nehamarlady <nehamarlady@gmail.com>
2026-05-24 12:07:22 -0700

    03 age

--- 06-javascript/03-age.js
+++ 06-javascript/03-age.js
@@ -1,5 +1,30 @@
-// Add your code here
+const calculateAge = function (dateString) {
+    const birthDate = new Date(dateString);
+    const today = new Date();
 
+    if(isNaN(birthDate.getTime())){
+        return 'Error: Invalid date format';
+    }
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthsDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    if(monthsDiff < 0 || (monthsDiff === 0 && dayDiff < 0)) age--;
+
+    if (age === 1){
+        return `You are ${age} year old`;
+    }
+    if (age > 125){
+        return `Are you sure you are more than 125 years old?`;
+    }
+    if (age < 0){
+        return `Error: Birth date cannot be in the future`;
+    }
+
+    return `You are ${age} years old`;
+
+}
 console.log(calculateAge('2000-07-01'));
 // You are 25 years old
 console.log(calculateAge('1988-05-18'));
