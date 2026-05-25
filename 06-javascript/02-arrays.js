6ca0b0a4c52f54e0bf9e9aa03ef8158ed1ac1856
nehamarlady <nehamarlady@gmail.com>
2026-05-24 23:58:44 -0700

    formatted

--- 06-javascript/02-arrays.js
+++ 06-javascript/02-arrays.js
@@ -1,34 +1,33 @@
 const pokemons = [
-  { id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison'] },
-  { id: 2, name: 'Ivysaur', types: ['Grass', 'Poison'] },
-  { id: 3, name: 'Venusaur', types: ['Grass', 'Poison'] },
-  { id: 4, name: 'Charmander', types: ['Fire'] },
-  { id: 5, name: 'Charmeleon', types: ['Fire'] },
-  { id: 6, name: 'Charizard', types: ['Fire', 'Flying'] },
-  { id: 7, name: 'Squirtle', types: ['Water'] },
-  { id: 8, name: 'Wartortle', types: ['Water'] },
-  { id: 9, name: 'Blastoise', types: ['Water'] },
-  { id: 10, name: 'Caterpie', types: ['Bug'] },
-  { id: 11, name: 'Metapod', types: ['Bug'] },
-  { id: 12, name: 'Butterfree', types: ['Bug', 'Flying'] },
-  { id: 13, name: 'Weedle', types: ['Bug', 'Poison'] },
-  { id: 14, name: 'Kakuna', types: ['Bug', 'Poison'] },
-  { id: 15, name: 'Beedrill', types: ['Bug', 'Poison'] },
-  { id: 16, name: 'Pidgey', types: ['Normal', 'Flying'] },
-  { id: 17, name: 'Pidgeotto', types: ['Normal', 'Flying'] },
-  { id: 18, name: 'Pidgeot', types: ['Normal', 'Flying'] },
-  { id: 19, name: 'Rattata', types: ['Normal'] },
-  { id: 20, name: 'Raticate', types: ['Normal'] },
+  { id: 1, name: "Bulbasaur", types: ["Grass", "Poison"] },
+  { id: 2, name: "Ivysaur", types: ["Grass", "Poison"] },
+  { id: 3, name: "Venusaur", types: ["Grass", "Poison"] },
+  { id: 4, name: "Charmander", types: ["Fire"] },
+  { id: 5, name: "Charmeleon", types: ["Fire"] },
+  { id: 6, name: "Charizard", types: ["Fire", "Flying"] },
+  { id: 7, name: "Squirtle", types: ["Water"] },
+  { id: 8, name: "Wartortle", types: ["Water"] },
+  { id: 9, name: "Blastoise", types: ["Water"] },
+  { id: 10, name: "Caterpie", types: ["Bug"] },
+  { id: 11, name: "Metapod", types: ["Bug"] },
+  { id: 12, name: "Butterfree", types: ["Bug", "Flying"] },
+  { id: 13, name: "Weedle", types: ["Bug", "Poison"] },
+  { id: 14, name: "Kakuna", types: ["Bug", "Poison"] },
+  { id: 15, name: "Beedrill", types: ["Bug", "Poison"] },
+  { id: 16, name: "Pidgey", types: ["Normal", "Flying"] },
+  { id: 17, name: "Pidgeotto", types: ["Normal", "Flying"] },
+  { id: 18, name: "Pidgeot", types: ["Normal", "Flying"] },
+  { id: 19, name: "Rattata", types: ["Normal"] },
+  { id: 20, name: "Raticate", types: ["Normal"] },
 ];
 
-const forEachPokemon= function (){
+const forEachPokemon = function () {
   pokemons.forEach((item) => {
-    console.log(`#${item.id} ${item.name} - ${item.types.join(' / ')}`);
-  })
+    console.log(`#${item.id} ${item.name} - ${item.types.join(" / ")}`);
+  });
+};
 
-}
-
-console.group('=========== forEachPokemon =========== ');
+console.group("=========== forEachPokemon =========== ");
 console.log(forEachPokemon());
 // #1 Bulbasaur - Grass / Poison
 // #2 Ivysaur - Grass / Poison
@@ -52,22 +51,21 @@ console.log(forEachPokemon());
 // #20 Raticate - Normal
 console.groupEnd();
 
-const filterPokemons = function (pokemonType){
+const filterPokemons = function (pokemonType) {
   const filterPokemons = pokemons
     .filter((item) => item.types.includes(pokemonType))
     .map((item) => item.name)
     .sort();
 
   return filterPokemons;
+};
 
-}
-
-console.group('=========== filterPokemons =========== ');
-console.log(filterPokemons('Fire'));
+console.group("=========== filterPokemons =========== ");
+console.log(filterPokemons("Fire"));
 // [ 'Charizard', 'Charmander', 'Charmeleon' ]
-console.log(filterPokemons('Normal'));
+console.log(filterPokemons("Normal"));
 // [ 'Pidgeot', 'Pidgeotto', 'Pidgey', 'Raticate', 'Rattata' ]
-console.log(filterPokemons('Poison'));
+console.log(filterPokemons("Poison"));
 // [ 'Beedrill', 'Bulbasaur', 'Ivysaur', 'Kakuna', 'Venusaur', 'Weedle' ]
 console.groupEnd();
 
@@ -78,24 +76,24 @@ const searchPokemons = function (searchQuery) {
     const searchName = item.name.toLowerCase().includes(query);
 
     const searchType = item.types.some((type) =>
-      type.toLowerCase().includes(query)
+      type.toLowerCase().includes(query),
     );
 
     return searchName || searchType;
   });
 
   if (resultsArray.length === 0) {
-    return 'No Pokémon with that name or type were found';
+    return "No Pokémon with that name or type were found";
   }
 
   return resultsArray;
 };
-console.group('=========== searchPokemons =========== ');
-console.log(searchPokemons('Wartortle'));
+console.group("=========== searchPokemons =========== ");
+console.log(searchPokemons("Wartortle"));
 // [ { id: 8, name: 'Wartortle', types: [ 'Water' ] } ]
-console.log(searchPokemons('pidgey'));
+console.log(searchPokemons("pidgey"));
 // [ { id: 16, name: 'Pidgey', types: [ 'Normal', 'Flying' ] } ]
-console.log(searchPokemons('bug'));
+console.log(searchPokemons("bug"));
 // [
 //   { id: 10, name: 'Caterpie', types: [ 'Bug' ] },
 //   { id: 11, name: 'Metapod', types: [ 'Bug' ] },
@@ -106,19 +104,19 @@ console.log(searchPokemons('bug'));
 // ]
 console.groupEnd();
 
-const reducePokemons = function() {
+const reducePokemons = function () {
   const result = pokemons.reduce((acc, pokemon) => {
-    pokemon.types.forEach(type => {
-      if (acc[type]) acc[type]++
+    pokemon.types.forEach((type) => {
+      if (acc[type]) acc[type]++;
       else acc[type] = 1;
-      });
+    });
 
-      return acc;
-    }, {});
+    return acc;
+  }, {});
 
   return result;
-}
-console.group('=========== reducePokemons =========== ');
+};
+console.group("=========== reducePokemons =========== ");
 console.log(reducePokemons());
 // {
 //   Grass: 3,
